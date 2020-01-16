368ce7429a89db7655d69c1368e30cb234219562
github username <github email address>
2020-01-16 11:29:25 -0700

    Mapping Ruby Classes to Database Tables

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,63 @@
 class Student
 
+  attr_accessor :name, :grade 
+  attr_reader :id 
+  
+  @@all = []
+
+  def initialize(name, grade, id = nil)
+    @name = name 
+    @grade = grade 
+    @id = id 
+   
+    @@all << self 
+  end 
+
+  def self.all 
+    @@all 
+  end 
+
+  def self.create_table 
+    sql = << -SQL 
+    CREATE TABLE students (
+      id INTEGER PRIMARY KEY,  
+      name TEXT, 
+      grade TEXT
+      )
+      SQL 
+    DB[:conn].execute(sql)
+  end 
+
+  def self.drop_table  
+    sql = << -SQL 
+    DROP TABLE students (
+      id INTEGER PRIMARY KEY,  
+      name TEXT, 
+      grade TEXT
+      )
+      SQL 
+    DB[:conn].execute(sql)
+  end
+  
+  def save 
+    sql = << -SQL 
+      INSERT INTO students (name, grade) 
+      VALUES (?, ?)
+    SQL 
+
+    DB[:conn].execute(sql, self.name, self.grade)
+
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM student")[0][0]
+  end 
+
+  def self.create(name, grade)
+    student = Student.new(name, grade)
+    student.save
+    student 
+  end
+
+
+  
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
